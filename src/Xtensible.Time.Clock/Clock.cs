e61add888d788890c07db7e8213a941524fc25e2
Gio Galbo <giovanni.galbo@mindbodyonline.com>
2019-06-16 14:09:34 -0400

    disallow null assignment to Clock.Default

--- src/Xtensible.Time.Clock/Clock.cs
+++ src/Xtensible.Time.Clock/Clock.cs
@@ -4,11 +4,12 @@ namespace Xtensible.Time
 {
 	public static class Clock
 	{
+		private static IClock _default = new WallClock();
+
 		/// <summary>
 		/// Switch default clock to MockClock for testing purposes (e.g. unit tests)
 		/// </summary>
-		public static IClock Default { get; set; } = new WallClock();
-
+		public static IClock Default { get => _default; set => _default = value ?? throw new ArgumentNullException(nameof(Default)); }
 		public static MockClock AsMockClock() => Default is MockClock ? Default as MockClock : new MockClock(DateTimeOffset.UtcNow);
 	}
 }
