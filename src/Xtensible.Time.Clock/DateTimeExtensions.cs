df966ff06fe489611b7629951b43c520bacc58e1
Gio Galbo <ggalbo@gmail.com>
2019-06-19 00:11:34 -0400

    Rounding functionality

---
+++ src/Xtensible.Time.Clock/DateTimeExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Xtensible.Time
+{
+	public static class DateTimeExtensions
+	{
+		public static DateTimeOffset RoundUp(this DateTimeOffset time, TimeSpan interval)
+		{
+			var modticks = time.Ticks % interval.Ticks;
+			var delta = modticks != 0 ? interval.Ticks - modticks : 0;
+			return new DateTimeOffset(time.Ticks + delta, TimeSpan.Zero);
+		}
+
+		public static DateTimeOffset RoundDown(this DateTimeOffset time, TimeSpan interval)
+		{
+			var delta = time.Ticks % interval.Ticks;
+			return new DateTimeOffset(time.Ticks - delta, TimeSpan.Zero);
+		}
+
+		public static DateTimeOffset RoundNearest(this DateTimeOffset time, TimeSpan interval)
+		{
+			var delta = time.Ticks % interval.Ticks;
+			bool roundUp = delta > interval.Ticks / 2;
+			var offset = roundUp ? interval.Ticks : 0;
+
+			return new DateTimeOffset(time.Ticks + offset - delta, TimeSpan.Zero);
+		}
+
+		public static DateTime RoundUp(this DateTime time, TimeSpan interval)
+		{
+			return RoundUp((DateTimeOffset)time, interval).DateTime;
+		}
+
+		public static DateTime RoundDown(this DateTime time, TimeSpan interval)
+		{
+			return RoundDown((DateTimeOffset)time, interval).DateTime;
+		}
+
+		public static DateTime RoundNearest(this DateTime time, TimeSpan interval)
+		{
+			return RoundNearest((DateTimeOffset)time, interval).DateTime;
+		}
+	}
+}
--- tests/Xtensible.Time.Tests/Tests.cs
+++ tests/Xtensible.Time.Tests/Tests.cs
@@ -29,5 +29,208 @@ namespace Xtensible.Time.Tests
 			_ = Assert.Throws<ArgumentNullException>(() => Clock.Default = null);
 		}
 
+		[Theory]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 5)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", (long) 1000 * 60 * 60  * 24)]
+		public void round_up_date_time_offset(string time, long intervalInMilliseconds)
+		{
+			var t = DateTimeOffset.Parse(time);
+			var r = t.RoundUp(TimeSpan.FromMilliseconds(intervalInMilliseconds));
+			Assert.True(r > t);
+
+			switch (intervalInMilliseconds)
+			{
+				case 1000:
+					Assert.Equal(17, r.Second);
+					break;
+				case 1000 * 5:
+					Assert.Equal(20, r.Second);
+					break;
+				case 1000 * 60:
+					Assert.Equal(23, r.Minute);
+					break;
+				case 1000 * 60 * 60:
+					Assert.Equal(8, r.Hour);
+					break;
+				case 1000 * 60 * 60 * 24:
+					Assert.Equal(2, r.Day);
+					break;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
+		[Theory]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 5)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", (long)1000 * 60 * 60 * 24)]
+		public void round_down_date_time_offset(string time, long intervalInMilliseconds)
+		{
+			var t = DateTimeOffset.Parse(time);
+			var r = t.RoundDown(TimeSpan.FromMilliseconds(intervalInMilliseconds));
+			Assert.True(r < t);
+
+			switch (intervalInMilliseconds)
+			{
+				case 1000:
+					Assert.Equal(16, r.Second);
+					break;
+				case 1000 * 5:
+					Assert.Equal(15, r.Second);
+					break;
+				case 1000 * 60:
+					Assert.Equal(22, r.Minute);
+					break;
+				case 1000 * 60 * 60:
+					Assert.Equal(7, r.Hour);
+					break;
+				case 1000 * 60 * 60 * 24:
+					Assert.Equal(1, r.Day);
+					break;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
+		[Theory]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 5)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", (long)1000 * 60 * 60 * 24)]
+		public void round_nearest_date_time_offset(string time, long intervalInMilliseconds)
+		{
+			var t = DateTimeOffset.Parse(time);
+			var r = t.RoundNearest(TimeSpan.FromMilliseconds(intervalInMilliseconds));
+			
+			switch (intervalInMilliseconds)
+			{
+				case 1000:
+					Assert.Equal(16, r.Second);
+					break;
+				case 1000 * 5:
+					Assert.Equal(15, r.Second);
+					break;
+				case 1000 * 60:
+					Assert.Equal(22, r.Minute);
+					break;
+				case 1000 * 60 * 60:
+					Assert.Equal(7, r.Hour);
+					break;
+				case 1000 * 60 * 60 * 24:
+					Assert.Equal(1, r.Day);
+					break;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
+		[Theory]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 5)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", (long)1000 * 60 * 60 * 24)]
+		public void round_up_date_time(string time, long intervalInMilliseconds)
+		{
+			var t = DateTimeOffset.Parse(time).DateTime;
+			var r = t.RoundUp(TimeSpan.FromMilliseconds(intervalInMilliseconds));
+			Assert.True(r > t);
+
+			switch (intervalInMilliseconds)
+			{
+				case 1000:
+					Assert.Equal(17, r.Second);
+					break;
+				case 1000 * 5:
+					Assert.Equal(20, r.Second);
+					break;
+				case 1000 * 60:
+					Assert.Equal(23, r.Minute);
+					break;
+				case 1000 * 60 * 60:
+					Assert.Equal(8, r.Hour);
+					break;
+				case 1000 * 60 * 60 * 24:
+					Assert.Equal(2, r.Day);
+					break;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
+		[Theory]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 5)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", (long)1000 * 60 * 60 * 24)]
+		public void round_down_date_time(string time, long intervalInMilliseconds)
+		{
+			var t = DateTimeOffset.Parse(time).DateTime;
+		
+			var r = t.RoundDown(TimeSpan.FromMilliseconds(intervalInMilliseconds));
+			Assert.True(r < t);
+
+			switch (intervalInMilliseconds)
+			{
+				case 1000:
+					Assert.Equal(16, r.Second);
+					break;
+				case 1000 * 5:
+					Assert.Equal(15, r.Second);
+					break;
+				case 1000 * 60:
+					Assert.Equal(22, r.Minute);
+					break;
+				case 1000 * 60 * 60:
+					Assert.Equal(7, r.Hour);
+					break;
+				case 1000 * 60 * 60 * 24:
+					Assert.Equal(1, r.Day);
+					break;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
+		[Theory]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 5)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", 1000 * 60 * 60)]
+		[InlineData("2019-07-01T07:22:16.3000000Z", (long)1000 * 60 * 60 * 24)]
+		public void round_nearest_date_time(string time, long intervalInMilliseconds)
+		{
+			var t = DateTimeOffset.Parse(time).DateTime;
+			var r = t.RoundNearest(TimeSpan.FromMilliseconds(intervalInMilliseconds));
+
+			switch (intervalInMilliseconds)
+			{
+				case 1000:
+					Assert.Equal(16, r.Second);
+					break;
+				case 1000 * 5:
+					Assert.Equal(15, r.Second);
+					break;
+				case 1000 * 60:
+					Assert.Equal(22, r.Minute);
+					break;
+				case 1000 * 60 * 60:
+					Assert.Equal(7, r.Hour);
+					break;
+				case 1000 * 60 * 60 * 24:
+					Assert.Equal(1, r.Day);
+					break;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
 	}
 }
